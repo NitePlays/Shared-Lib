3b2df04776358f38ed6a72af5d3239c168bed06b
NitePlays <126669908+NitePlays@users.noreply.github.com>
2023-05-08 09:26:24 +0300

    Update CreateDocker.groovy

--- vars/CreateDocker.groovy
+++ vars/CreateDocker.groovy
@@ -1,7 +1,3 @@
-def CreateDocker(String IMAGE_NAME, String BUILD_NUMBER, String TYPE) {
-  stage('Create Docker Image') {
-              steps {
-                sh "docker build -t ${IMAGE_NAME}:${TYPE}_${BUILD_NUMBER} ."
-            }
-        }
+def call(IMAGE_NAME, BUILD_NUMBER, TYPE) {
+      sh "docker build -t ${IMAGE_NAME}:${TYPE}_${BUILD_NUMBER} ."
 }
