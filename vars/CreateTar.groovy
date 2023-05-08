b24e68a74b697afc3bb175d8625784f83514e994
NitePlays <126669908+NitePlays@users.noreply.github.com>
2023-05-08 09:28:46 +0300

    Update CreateTar.groovy

--- vars/CreateTar.groovy
+++ vars/CreateTar.groovy
@@ -1,9 +1,5 @@
-def CreateTar() {
-          stage('Create Tar Archive') {
-              steps {
+def call() {
                 sh 'tar -czf ../workspace.tar.gz --exclude=node_modules --exclude=README.md --directory=.. workspace'
                 sh 'mkdir -p ../archives'
                 sh 'mv ../workspace.tar.gz ../archives'
-              }
-            }
 }
