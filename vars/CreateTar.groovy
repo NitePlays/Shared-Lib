8f9700f94699b34be195a1e58e6591fcf080b69d
NitePlays <126669908+NitePlays@users.noreply.github.com>
2023-05-10 11:59:43 +0300

    Update CreateTar.groovy

--- vars/CreateTar.groovy
+++ vars/CreateTar.groovy
@@ -1,5 +1,4 @@
 def call(TYPE, BUILD_NUMBER) {
                 sh 'tar -czf ${TYPE}_${BUILD_NUMBER}.tar.gz --exclude=node_modules --exclude=README.md .'
-                sh 'mkdir -p ../archives'
                 sh 'mv ${TYPE}_${BUILD_NUMBER}.tar.gz ../archives'
 }
