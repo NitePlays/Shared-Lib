d6755989ba2c62d2e64c7da5e2a8c4f78c025d1e
NitePlays <126669908+NitePlays@users.noreply.github.com>
2023-05-10 10:47:46 +0300

    Update Trivy.groovy

--- vars/Trivy.groovy
+++ vars/Trivy.groovy
@@ -3,9 +3,8 @@ def call(IMAGE_NAME, BUILD_NUMBER, TYPE) {
         sh "trivy image --format json ${IMAGE_NAME}:${TYPE}_${BUILD_NUMBER} > vuln.json"
         def vulnerabilities = readFile file: 'vuln.json'
         
-        vulnerabilities.each { vulnerability ->
-            if (vulnerability.severity == 'CRITICAL') {
-                error "Critical vulnerability found: ${vulnerability.vulnerability}"
+            if (vulnerabilities_contain == 'CRITICAL') {
+                error "Critical vulnerability found"
             }
         }
     }
