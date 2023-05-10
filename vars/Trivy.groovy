b2bad8982dee6483dec112c0963da9e0a153b873
NitePlays <126669908+NitePlays@users.noreply.github.com>
2023-05-10 10:23:27 +0300

    Update Trivy.groovy

--- vars/Trivy.groovy
+++ vars/Trivy.groovy
@@ -2,7 +2,8 @@ def call(IMAGE_NAME, BUILD_NUMBER, TYPE) {
     script {
         sh "trivy image --format json ${IMAGE_NAME}:${TYPE}_${BUILD_NUMBER} > vuln.json"
         def vulnerabilities = readFile file: 'vuln.json'
-        def json = readJSON text: vulnerabilities
+        def slurper = new JsonSlurper()
+        def json = slurper.parseText(vulnerabilities)
         
         json.each { vulnerability ->
             if (vulnerability.severity == 'CRITICAL') {
