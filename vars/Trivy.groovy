b4cabd2bce3ffb626f8fccbaf50f7fde23a2f94f
NitePlays <126669908+NitePlays@users.noreply.github.com>
2023-05-10 10:02:00 +0300

    Update Trivy.groovy

--- vars/Trivy.groovy
+++ vars/Trivy.groovy
@@ -1,6 +1,6 @@
 def call(IMAGE_NAME, BUILD_NUMBER, TYPE) {
                 script {
-                    def trivy_output = sh(script: "trivy image ${IMAGE_NAME}:${TYPE}_${BUILD_NUMBER}")
+                    def trivy_output = sh(script: "trivy image --format json ${IMAGE_NAME}:${TYPE}_${BUILD_NUMBER} | grep "Severity"")
                     if (trivy_output.contains('CRITICAL')) {
                         error('Critical vulnerabilities found in Docker image.')
                     }
