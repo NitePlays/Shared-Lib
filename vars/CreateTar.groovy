9079d3821153b49949501cbfed82f5ac02a7eef9
NitePlays <126669908+NitePlays@users.noreply.github.com>
2023-05-23 23:33:37 +0300

    Delete CreateTar.groovy

--- vars/CreateTar.groovy
+++
@@ -1,5 +0,0 @@
-def call(TYPE, BUILD_NUMBER) {
-    dir('../archives') {
-        sh "sudo tar -czf ${TYPE}_${BUILD_NUMBER}.tar.gz --exclude=node_modules --exclude=README.md --directory=../${TYPE}Song ."
-    }
-}
