d5b6f6bf4fe575525db18bab23c2ef61eadb1cfa
NitePlays <126669908+NitePlays@users.noreply.github.com>
2023-05-23 23:34:14 +0300

    Delete PushToECR.groovy

--- vars/PushToECR.groovy
+++
@@ -1,7 +0,0 @@
-def call(ECR_REGISTRY, IMAGE_NAME, DOCKER_IMAGE, TYPE) {
-                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws3']]) {
-                    sh "docker login -u AWS -p \$(aws ecr get-login-password) ${ECR_REGISTRY}"
-                    sh "docker tag ${IMAGE_NAME}:${TYPE}_${BUILD_NUMBER} ${DOCKER_IMAGE}"
-                    sh "docker push ${DOCKER_IMAGE}"
-        }
-}
